bc74eb389224c4d3dfb7f1021cfe830b614e0862
Nicholas Nolen <nicnolen@ymail.com>
2021-12-10 14:25:21 -0600

    added in OpenWeather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,53 @@
 // GLOBAL VARIABLES
 // Reference the form
-userFormEl = document.getElementById('city-form');
+var userFormEl = document.getElementById('city-form');
 // Reference the input box
-userInputEl = document.querySelector('.city-input');
+var userInputEl = document.querySelector('.city-input');
 // Reference the submit button
-searchBtnEl = document.getElementById('search-btn');
+var searchBtnEl = document.getElementById('search-btn');
 // Reference the container where the weather info will be stored
-weatherInfoEl = document.getElementById('weather-info');
+var weatherInfoEl = document.getElementById('weather-info');
+
+// define the OpenWeatherMap API URL
+var apiUrl =
+  'https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey)';
+// Reference your API key. The API  is your unique id associated with your OpenWeatherMap account
+var apiKey = '164ca084a373d5791ba7dbbc5cff2467';
+
+// FUNCTIONS
+// Function when user submits form
+
+// Function to display current weather
+var currentWeather = function (weather) {
+  // grab the current weather data
+  // pass the OpenWeatherMap URL to the fetch method to access the url
+  // the fetch() method returns a promise containing the response object (response parameter)
+  fetch(apiUrl).then(function (response) {
+    // request as successful (.ok)
+    if (response.ok) {
+      console.log(response);
+      // to make the data (data parameter) useful, convert it to a JSON format using .json()
+      response
+        .json()
+        .then(function (data) {
+          console.log(data.items, weather);
+        })
+        // request was unsuccessful
+        .catch(function () {
+          alert('Please search for a valid city');
+        });
+    }
+  });
+};
+currentWeather();
+
+// Function to display 5 day weather forecast
+var futureWeather = function () {
+  console.log('future weather');
+};
+futureWeather();
+
+// CLICK EVENTS
+// Search button click event to save city
+// userFormEl.addEventListener('submit', formSubmitHandler);
+// searchBtnEl.addEventListener('click' buttonClickHandler);
