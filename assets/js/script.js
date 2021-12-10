e1ea64ba887015bdd592d50a321c92bf68650687
Nicholas Nolen <nicnolen@ymail.com>
2021-12-10 11:03:45 -0600

    added in a city search form and styled with bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-
+// GLOBAL VARIABLES
+// Reference the form
+userFormEl = document.getElementById('city-form');
+// Reference the input box
+userInputEl = document.querySelector('.city-input');
+// Reference the submit button
+searchBtnEl = document.getElementById('search-btn');
+// Reference the container where the weather info will be stored
+weatherInfoEl = document.getElementById('weather-info');
